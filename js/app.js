ebaec041412a2a73702cb18651fe25256f619d01
doug <drcox23@gmail.com>
2018-08-01 22:00:12 -1000

    initial commit. have things showing up, now just need to do edge cases

---
+++ js/app.js
@@ -0,0 +1,56 @@
+console.log("sanity");
+
+let redditURL = "https://www.reddit.com/r/aww.json";
+
+elementCreator = (elem, myClass, parent, content) => {
+  const newElem = document.createElement(elem);
+  newElem.className = myClass;
+  parent.appendChild(newElem);
+  if (content) {
+    newElem.innerHTML = content;
+  }
+  return newElem;
+};
+
+mediaCreator = (myClass, parent, link) => {
+  const newMedia = document.createElement("img");
+  newMedia.className = myClass;
+  parent.appendChild(newMedia);
+  newMedia.src = link;
+};
+
+const getRekd = (url, callback) => {
+  const oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", callback);
+  oReq.open("GET", url);
+  oReq.send();
+};
+
+firstCall = getRekd(redditURL, res => {
+  const callSub = JSON.parse(res.currentTarget.response);
+  let posts = callSub.data.children;
+  console.log("posts: ", posts);
+
+  for (let i = 0; i < posts.length; i++) {
+    console.log(posts[i]);
+    console.log(posts[i].data.title);
+
+    let redditItem = elementCreator("div", "redPost", company);
+    let postTitle = elementCreator(
+      "h2",
+      "redTitle",
+      redditItem,
+      posts[i].data.title
+    );
+    let postInfo = elementCreator(
+      "div",
+      "postInfo",
+      postTitle,
+      "by: " +
+        posts[i].data.author +
+        " • " +
+        posts[i].data.subreddit_name_prefixed
+    );
+    let postMedia = mediaCreator("postMedia", postTitle, posts[i].data.url);
+  }
+});
